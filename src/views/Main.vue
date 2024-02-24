120901414ae3072f22219a695ca54f77922d839f
alan <sz9751210@gmail.com>
2024-02-24 13:00:30 +0800

    安裝 vue-router

---
+++ src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>左側菜單</div>
+  <div>header</div>
+  <div>
+    Main Content
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
