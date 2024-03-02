977f228392dd4c68258077ff2e84583d6cb7a86c
alan <sz9751210@gmail.com>
2024-03-02 16:52:35 +0800

    新增user 表單樣式完成

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="user-header">
-    <el-button type="primary">＋新增</el-button>
+    <el-button type="primary" @click="dialogVisible = true">＋新增</el-button>
     <el-form :inline="true" :model="formInline">
-    <el-form-item label="請輸入">
-      <el-input v-model="formInline.keyword" placeholder="請輸入用戶名" clearable />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="handleSearch">搜尋</el-button>
-    </el-form-item>
-  </el-form>
+      <el-form-item label="請輸入">
+        <el-input
+          v-model="formInline.keyword"
+          placeholder="請輸入用戶名"
+          clearable
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleSearch">搜尋</el-button>
+      </el-form-item>
+    </el-form>
   </div>
   <div class="table">
     <el-table :data="list" style="width: 100%" height="600px">
@@ -35,6 +39,77 @@
       class="pager mt-4"
     />
   </div>
+  <el-dialog
+    v-model="dialogVisible"
+    title="新增用戶"
+    width="45%"
+    :before-close="handleClose"
+  >
+    <el-form :inline="true" :model="formUser">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="姓名">
+            <el-input
+              v-model="formUser.name"
+              placeholder="請輸入姓名"
+              clearable
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="年齡">
+            <el-input
+              v-model="formUser.age"
+              placeholder="請輸入年齡"
+              clearable
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="性別">
+            <el-select
+              v-model="formUser.sex"
+              placeholder="請選擇"
+              style="width: 170px"
+            >
+              <el-option label="男" value="0" />
+              <el-option label="女" value="1" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="出生日期">
+            <el-date-picker
+              v-model="formUser.birth"
+              type="date"
+              placeholder="請輸入日期"
+              clearable
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-form-item label="地址">
+          <el-input
+            v-model="formUser.addr"
+            placeholder="請輸入地址"
+            clearable
+          />
+        </el-form-item>
+      </el-row>
+      <el-row style="justify-content: flex-end;">
+        <el-form-item>
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">
+            確定
+          </el-button>
+        </el-form-item>
+      </el-row>
+    </el-form>
+  </el-dialog>
 </template>
 
 <script setup>
@@ -74,8 +149,29 @@ const formInline = reactive({
   keyword: "",
 });
 const handleSearch = () => {
-  config.name = formInline.keyword
+  config.name = formInline.keyword;
   getUserData(config);
+};
+
+const dialogVisible = ref(false);
+const handleClose = (done) => {
+  ElMessageBox.confirm("確定關閉嗎?")
+    .then(() => {
+      done();
+    })
+    .catch(() => {
+      // catch error
+    });
+};
+const formUser = reactive({
+  name: "",
+  age: "",
+  sex: "",
+  birth: "",
+  date: "",
+});
+const onSubmit = () => {
+  console.log("submit!");
 };
 
 onMounted(() => {
@@ -92,9 +188,13 @@ onMounted(() => {
     right: 0;
     bottom: -120px;
   }
-}.user-header{
+}
+.user-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+// .el-select {
+//   --el-select-width: 170px;
+// }
 </style>
